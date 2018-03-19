6918b22f3a031f6ecb8a9d793642d91f123a4765
Danwang <danwang@lagou.com>
2018-03-19 18:56:23 +0800

    move

--- js/dan.js
+++ js/dan.js
@@ -24,33 +24,6 @@ function $(ele) {
 }
 /*
   author:dan
-  time: 2018-01-15
-  usage: 获取一个元素
-  params: 
-   1.获取一个元素 ele
-   2.获取一个属性 attr
-   3.方向 dir（步进速度）
-   4.秒数 seconds
-   5.执行完当前之后的回调
-*/
-function doMove ( obj, attr, dir, target, seconds, endFn ) {
-  dir = parseInt(getStyle( obj, attr )) < target ? dir : -dir;
-  clearInterval( obj.timer );
-  obj.timer = setInterval(function () { 
-    var speed = parseInt(getStyle( obj, attr )) + dir;      // 步长
-    if ( speed > target && dir > 0 ||  speed < target && dir < 0  ) {
-      speed = target;
-    }
-    obj.style[attr] = speed + 'px';
-    if ( speed == target ) {
-      clearInterval( obj.timer );
-      endFn && endFn();
-      
-    }
-  }, seconds);
-}
-/*
-  author:dan
   time: 2018-01-30
   usage: 判断一个字符串是不是全都是数字
   params: 
